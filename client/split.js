f4b837c2e861e9cfd2814a99a44adff07fd99525
lenasano <114319916+lenasano@users.noreply.github.com>
2023-11-27 05:48:33 -0800

    Measure the Split SDK initialization time
    * Measure the time to initialize the Split SDK (including time to download the Split payload: Split feature flag and segment definitions) until the SDK_READY event is received by the factory client.
    * Refactor the client-side code (to reduce duplicate code).
    * Await the client.ready() state on the server.
    * Remove debug logging on the client and update README.

---
+++ client/split.js
@@ -0,0 +1,82 @@
+// This implementation of the singleton pattern ensures that only one instance of the
+// SplitFactory client is created on the web client. This means that only one copy of
+// the Split (feature flag and segment) definitions are downloaded and synchronized.
+
+const { Timer } = require('./../utils/timer');
+let client;
+
+/**
+ * Create (instantiate) a SplitSuite (Split SDK) client. The client is used to track events and evaluate feature flags.
+ *
+ * NOTE: This method creates a single instance (singleton) of the Split SDK client.
+ *
+ * @function getSplitClient
+ * @returns {SplitIO.IClient} The singleton instance of the ISuiteSDK.client()
+ */
+const getSplitClient = async () => await getSplitClientForFlagset();
+
+/**
+ * Create (instantiate) a SplitSuite (Split SDK) client. The client is used to track events and evaluate feature flags.
+ *
+ * NOTE: This method creates a single instance (singleton) of the Split SDK client.
+ *
+ * @function getSplitClientForFlagset
+ * 
+ * @param {string} flagsetname
+ * The name of the flag set already associated with feature flags in Split cloud (this can be done in Split UI).
+ * If provided, the flag set name is used to filter the flags synchronized from Split cloud, resulting in a smaller payload.
+ * 
+ * @returns {SplitIO.IClient} 
+ * The singleton instance of the ISuiteSDK.client()
+ */
+async function getSplitClientForFlagset(flagsetname = '') {
+
+    const trafficType = process.env.TRAFFIC_TYPE;
+
+    if (!client) {
+
+        // Dynamically import a local module, which in turn imports '@splitsoftware/browser-split-suite' for tree-shaking, resulting in a smaller app
+        const { SplitSuite /*, DebugLogger*/ } = await import('./browser-split-suite');
+
+        let config = {
+            core: {
+                authorizationKey: process.env.CLIENT_SIDE_SDK_KEY,
+            
+                // In this example, we get the user key from URL query parameter `id`
+                key: new URLSearchParams(window.location.search).get('id'),
+                // Specifying the traffic type for the user key is optional, the value is 'user' by default
+                trafficType: trafficType
+
+            }//,
+            //debug: DebugLogger()
+        };
+
+        if(flagsetname !== ''){
+            config.sync = {
+                splitFilters: [{
+                    type: 'bySet',
+                    values: [flagsetname]
+                }]
+            };
+        }
+
+        const timer = Timer();
+
+        client = SplitSuite(config).client();
+        client.on(client.Event.SDK_READY, function() {
+
+            // send latency (SDK initialization time, including time downloading Split definitions) to Split cloud as a custom event
+            client.track(trafficType, `splitsdk.${ client.Event.SDK_READY.replaceAll('::', '_') }`, timer.duration());
+
+            console.log(`Split SDK initialized in ${timer.duration()} ms`)
+        });
+        
+        return await client;
+
+    } else {
+
+        return client;
+    }
+}
+
+module.exports = { getSplitClient, getSplitClientForFlagset };
